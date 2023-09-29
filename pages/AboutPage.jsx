072939a9b2136135c30f10688ea934aaa363a158
Piyush Sharma <118152296+pspiyush130@users.noreply.github.com>
2023-09-29 23:37:56 +0530

    v1

---
+++ pages/AboutPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
+          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
+          totam vero quas provident ipsam, veritatis nostrum velit quos
+          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
+          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
+          porro tempore quis eaque labore voluptatibus ea necessitatibus
+          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
+          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
+          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
+          delectus expedita a alias nam recusandae illo debitis repellat libero,
+          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
+          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://i.pinimg.com/originals/c7/57/45/c75745421f5356ff016a1727acd4bf24.jpg" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">MEN</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://www.nextmsc.com/uploads/images/Things-You-Should-Know-about-Fast-Fashion.jpg" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">WOMEN</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://th.bing.com/th/id/OIP.MzTzW4u4ByZEuSAiYAvLRgHaE8?w=271&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">FatFashion</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
---
+++ pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Navbar, Main,  Footer,Product } from "../components";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Product />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
